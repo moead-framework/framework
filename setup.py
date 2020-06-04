65f1450883288a94fd54b93b744bcc3786ae4daa
Geoffrey Pruvost <geoffrey@pruvost.xyz>
2020-06-04 16:06:21 +0200

    0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="moead-framework",
-    version="0.0.3",
+    version="0.0.4",
     author="Geoffrey Pruvost",
     author_email="geoffrey@pruvost.xyz",
     description="MOEA/D Framework in Python 3",
