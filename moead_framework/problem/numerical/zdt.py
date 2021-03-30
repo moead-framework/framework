69afbd94b1976d7c036e648b9f8e068e35c94b31
Geoffrey Pruvost <geoffrey@pruvost.xyz>
2021-03-30 18:03:56 +0200

    issue #42 : raise exception in zdt1 if size <= 1

--- moead_framework/problem/numerical/zdt.py
+++ moead_framework/problem/numerical/zdt.py
@@ -20,6 +20,10 @@ class Zdt1(Problem):
         :param size: {integer} number of variables in a solution
         """
         self.n = size
+
+        if self.n <= 1:
+            raise ValueError("The parameter 'size' must be an integer strictly greater than 1 (size > 1)")
+
         super().__init__()
 
     def f(self, function_id, solution):
